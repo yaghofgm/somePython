49fcf70966d5da110927aba74223ee715e755403
Yagho Grossi <yaghogrossi@gmail.com>
2025-03-21 14:41:09 -0400

    m

--- random/plot_graph.py
+++ random/plot_graph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f1(t):
-    return np.exp(-500*t)*(-8*np.cos(866*t)-25.4*np.sin(866*t))+2
+    return np.exp(-500*t)*(-8*np.cos(866*t)-4.62*np.sin(866*t))+2
 
 x=np.linspace(0,0.02,1000)
 y1=f1(x)
